60b4b1b171a57d81f98b7ddbc272f12daa5ac39c
ckseeni <ckseeni199631@gmail.com>
2018-10-04 18:56:34 +0530

    added functionality

--- src/main/java/com/ExpTracker/Controller/MainController.java
+++ src/main/java/com/ExpTracker/Controller/MainController.java
@@ -1,8 +1,12 @@
 package com.ExpTracker.Controller;
 
+
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class MainController {
@@ -10,6 +14,13 @@ public class MainController {
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String indexPage() {
 		return "index";
+	}
+	
+	@RequestMapping(value = "/updateExp", method = RequestMethod.GET, produces = {"application/json"})
+	public @ResponseBody Object updateExpenses() {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put ("msg", "dontknow");
+		return jsonObject.toString();
 	}
 
 }
