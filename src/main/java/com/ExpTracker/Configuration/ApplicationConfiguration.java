142c645931452f178eee95915cd5dc4bf4b998c4
Seenivasan Krishnan <Seenivasan.Krishnan@genesys.com>
2018-10-04 11:08:09 +0530

    first commit

---
+++ src/main/java/com/ExpTracker/Configuration/ApplicationConfiguration.java
@@ -0,0 +1,29 @@
+package com.ExpTracker.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.ExpTracker")
+public class ApplicationConfiguration implements WebMvcConfigurer {
+	
+	//@Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+	
+    @Bean
+    public InternalResourceViewResolver htmlViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/html/");
+        bean.setSuffix(".html");
+        return bean;
+    }
+    
+}
