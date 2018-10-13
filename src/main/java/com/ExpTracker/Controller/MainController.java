0d5e1f16440a29091ed23b43f55932d514b335c1
ckseeni <ckseeni199631@gmail.com>
2018-10-13 18:49:30 +0530

    hibernate

--- src/main/java/com/ExpTracker/Controller/MainController.java
+++ src/main/java/com/ExpTracker/Controller/MainController.java
@@ -2,12 +2,21 @@ package com.ExpTracker.Controller;
 
 
 
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;  
+import org.hibernate.SessionFactory;  
+
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 
 @Controller
@@ -21,6 +30,15 @@ public class MainController {
 	@RequestMapping(value = "/updateExp", method = RequestMethod.POST)
 	public @ResponseBody String updateExpenses(@RequestBody String expData) {
 		JSONObject jsonObject = new JSONObject(expData);
+		@SuppressWarnings("deprecation")
+		SessionFactory factory=new AnnotationConfiguration().configure()
+				.buildSessionFactory();;      
+		//creating session object  
+		Session session=factory.openSession();  
+		Query query = session.createQuery("from Expenses");
+		List arr = query.list();
+		for(int i=0;i<arr.size();i++)
+			System.out.println(arr.get(i));
 		return jsonObject.toString();
 	}
 
---
+++ src/main/java/com/ExpTracker/Model/Expenses.java
@@ -0,0 +1,34 @@
+package com.ExpTracker.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Expenses")
+public class Expenses {
+
+	private String name;
+	private int amount;
+	
+	@Id
+	@Column(name="name")
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name="amount")
+	public int getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+}
