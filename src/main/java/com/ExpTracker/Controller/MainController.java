25d30de70acbf74342a0c8066f5c02204bba1575
ckseeni <ckseeni199631@gmail.com>
2018-10-16 20:14:27 +0530

    fix for query deprecation

--- src/main/java/com/ExpTracker/Controller/MainController.java
+++ src/main/java/com/ExpTracker/Controller/MainController.java
@@ -4,8 +4,7 @@ package com.ExpTracker.Controller;
 
 
 import java.util.List;
-
-import org.hibernate.Query;
+import javax.persistence.TypedQuery;
 import org.hibernate.Session;  
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.ExpTracker.Model.Expenses;
 
 
@@ -33,7 +31,6 @@ public class MainController {
 	
 	@RequestMapping(value = "/updateExp", method = RequestMethod.POST)
 	public @ResponseBody String updateExpenses(@RequestBody String expData) {
-		long startTime = System.currentTimeMillis();
 		JSONObject jsonObject = new JSONObject(expData);
 		SessionFactory factory  = null;
 		try {
@@ -44,17 +41,16 @@ public class MainController {
 		catch (Throwable th) {
 			System.err.println("Enitial SessionFactory creation failed" + th);
 			throw new ExceptionInInitializerError(th);
-		}
-		//creating session object  
+		}  
 		Session session=factory.openSession();  
-		Query query = session.createQuery("from Expenses");
-		List arr = query.list();
+		TypedQuery<Expenses> query = session.createQuery("from Expenses");
+		List<Expenses> arr = query.getResultList();
 		for(int i=0;i<arr.size();i++) {
-			Expenses e = (Expenses)arr.get(i);
+			Expenses e = arr.get(i);
 			System.out.println(e.getName());
 		}	
 		long endTime = System.currentTimeMillis() -startTime;
-		System.out.println(endTime);
+		session.close();
 		return jsonObject.toString();
 	}
 
